b256c82b7581c7d31031a4f5fb6efa16d77aee52
Eunkyung Ma <unmelted@naver.com>
2022-01-25 15:35:35 +0900

    necessary directory create

---
+++ pull_src.sh
@@ -0,0 +1,4 @@
+cd /Users/4dreplay/work/vespa
+git pull origin master
+#cp /Users/4dreplay/work/vespa/appcore/App/Zion/CMakeLists.txt /Users/4dreplay/work/zion/
+cp -r  /Users/4dreplay/work/vespa/appcore/App/Zion/src/ /Users/4dreplay/work/zion/src/
